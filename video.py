9e9da9318745018df663c94f79fecb099ae937f3
AroraKaran19 <karora0319@gmail.com>
2023-03-31 21:07:57 +0530

    added clip duration label

--- video.py
+++ video.py
@@ -9,9 +9,9 @@ def create_video():
 def browsing():
     file_path = filedialog.askopenfilename(initialdir="/", filetypes =[('MP4', '*.mp4'), ('MOV', '*.mov'), ('AVI', '*.avi'), ('MKV', "*.mkv"), ("All", "*.*")])
     if os.path.exists(file_path):
-        address.set(file_path)
         cap = cv.VideoCapture(file_path)
         if not cap.isOpened():
+            address.set("ERROR!!")
             messagebox.showerror("Error!", "Error opening video file!")
         else:
             ret, frame = cap.read()
@@ -21,14 +21,20 @@ def browsing():
             else:
                 # If the video file is not corrupt
                 from moviepy.video.io.VideoFileClip import VideoFileClip
+                address.set(file_path)
                 clip = VideoFileClip(file_path)
                 duration = [0, 0, 0]
-                clip_duration = clip.duration
-                while clip_duration != 0:
-                    if clip_duration > 59:
-                        
+                duration[2] = int(clip.duration)
+                while duration[2] > 59:
+                    if duration[2] > 59:
+                        duration[1]+=1
+                        duration[2]-=60
+                while duration[1] > 59:
+                    if duration[1] > 59:
+                        duration[0]+=1
+                        duration[1]-=60
+                duration = f"{duration[0]} hours {duration[1]} mins {duration[2]} secs"
                 duration_label.config(text=f"Duration: {duration}")
-                print(clip.duration)
                 cap.release()
 
 def new_background_add():
@@ -54,7 +60,7 @@ def new_background_add():
     bg_canvas.create_window(430, 100, window=browse)
 
     duration_label = Label(bg_canvas, text="Duration: ", font=('Minion Pro', 12), bg="white")
-    bg_canvas.create_window(200, 140, window=duration_label)
+    bg_canvas.create_window(240, 140, window=duration_label)
 
     root.eval('tk::PlaceWindow . center')
     root.mainloop()
@@ -74,4 +80,5 @@ if __name__ == "__main__":
                 import moviepy.editor as mpe
             else:
                 messagebox.showerror("Error!!", "Error Occured!\nReport on issues section\nhttps://github.com/AroraKaran19/gpt-to-shorts/issues")
+
     new_background_add()

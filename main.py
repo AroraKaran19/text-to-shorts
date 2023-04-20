bb3e6460e282b69e76a474a13d294cde6724d007
Karan Arora <karora0319@gmail.com>
2023-04-21 04:42:28 +0530

    Implemented New Look for Home GUI

--- main.py
+++ main.py
@@ -1,33 +1,19 @@
 import os
 import subprocess
 import sqlite3
-from tkinter import *
-from tkinter.messagebox import *
 from random import choice
-try:
-    import praw
-    import prawcore
-except ImportError:
-    opt = askokcancel(
-        "Install Library", "This Application requires python 'praw' library\nDo you wish to install it?")
-    if opt:
-        result = subprocess.run(
-            ["pip", "install", "praw"], capture_output=True)
-        if result:
-            import praw
-            import prawcore
-            showinfo("Install Library", "(!) Successfully Installed (!)")
-        else:
-            showerror("Error", "Error!")
 
-background = "#f0e68c"
+background = "white"
 db_name = "reddit_info.db"
 table = "users"
 
-
-def create_video():
+def shorts_generator(post_title, post_content):
     """ Generates Video """
-
+    audio=video.generate_audio(post_title.replace("?",""    ), post_content)
+    content=post_title+post_content
+    content= "\n".join([" ".join(content.split()[i:i+18]) for i in range(0, len(content.split()), 18)])
+    video.create_video(audio, content)
+    
 
 def fetch_post(community, post_id):
     """ Fetches Post """
@@ -37,67 +23,64 @@ def fetch_post(community, post_id):
     content = c.fetchone()
     conn.close()
     if content is not None:
-        client_id, client_secret = content[0].replace(
-            '\n', ''), content[1].replace('\t', '')
+        client_id, client_secret = content[0].replace('\n', ''), content[1].replace('\t', '')
     reddit = praw.Reddit(client_id=client_id,
-                         client_secret=client_secret,
-                         user_agent='ytshorts')
+                     client_secret=client_secret,
+                    user_agent='ytshorts')
     try:
         subreddit = reddit.subreddit(community)
         if post_id in ('', ' '):
             post_id = choice(list(subreddit.new()))
-
+    
         post = reddit.submission(id=post_id)
 
+        post_title=post.title
+        post_content=post.selftext
         print("------------------------------------------------")
-        print("\n(!) Title: ", post.title)
-        print("\n(!) Content: ", post.selftext)
+        print("\n(!) Title: ", post_title)
+        print("\n(!) Content: ", post_content)
         print(f"\n(!) Post Link: https://reddit.com/r/{community}/comments/{post_id}")
         print("\n------------------------------------------------")
-        create_video()
-
-    except:
-        showerror("Invalid Community",
-                  "Enter Appropriate Community Name or\nEntered community doesn't exists!")
-
+        shorts_generator(post_title, post_content)
+        
+    except prawcore.exceptions.Redirect or ValueError:
+        showerror("Invalid Community", "Enter Appropriate Community Name or\nEntered community doesn't exists!")
 
 def reddit_gui():
     """ GUI for Reddit Post Scrapping """
-    reddit_root = Tk()
-    reddit_root.title(" ൠ    Reddit Scrapper")
-    reddit_root.resizable(False, False)
-
-    canvas = Canvas(reddit_root, height=550, width=700, bg=background)
-    canvas.pack()
-
-    title = Label(canvas, text="Post Scrapper", bg=background,
-                  font=("Adobe Garamond Pro", 18, "bold"))
-    canvas.create_window(350, 130, window=title)
-
-    reddit_logo = PhotoImage(file=os.path.join("res", "reddit.png"))
-    logo_label = Label(canvas, image=reddit_logo, bg=background)
-    canvas.create_window(350, 60, window=logo_label)
-
-    community_label = Label(canvas, text="Community:", bg=background, font=(
-        "Adobe Garamond Pro", 12, "bold"))
-    canvas.create_window(195, 330, window=community_label)
-    subreddit_community = Entry(canvas, width=25, font=('', 14))
-    canvas.create_window(400, 330, window=subreddit_community)
-
-    post_label = Label(canvas, text="Post ID:", bg=background,
-                       font=("Adobe Garamond Pro", 12, "bold"))
-    canvas.create_window(210, 380, window=post_label)
-    post_id = Entry(canvas, width=25, font=('', 14))
-    canvas.create_window(400, 380, window=post_id)
-
-    create_button = Button(canvas, text="Create Video", bg="lime", font=(
-        "Adobe Garamond Pro", 12, "bold"), pady=8, command=lambda: fetch_post(subreddit_community.get(), post_id.get()))
-    canvas.create_window(350, 450, window=create_button)
-
-    reddit_root.eval('tk::PlaceWindow . center')
-    reddit_root.mainloop()
-
-
+    if os.path.exists("reddit_info.db"):
+        reddit_root = Tk()
+        reddit_root.title(" ൠ    Reddit Scrapper")
+        reddit_root.resizable(False, False)
+        
+        canvas = Canvas(reddit_root, height=550, width=700, bg=background)
+        canvas.pack()
+        
+        title = Label(canvas, text="Post Scrapper", bg=background, font=("Adobe Garamond Pro", 18, "bold"))
+        canvas.create_window(350, 130, window=title)
+        
+        reddit_logo = ImageTk.PhotoImage(file=os.path.join("res", "reddit.png"))
+        logo_label = Label(canvas, image=reddit_logo, bg=background)
+        canvas.create_window(350, 60, window=logo_label)
+        
+        community_label = Label(canvas, text="Community:", bg=background, font=("Adobe Garamond Pro", 12, "bold"))
+        canvas.create_window(195, 330, window=community_label)
+        subreddit_community = Entry(canvas, width=25, font=('', 14))
+        canvas.create_window(400, 330, window=subreddit_community)
+        
+        post_label = Label(canvas, text="Post ID:", bg=background, font=("Adobe Garamond Pro", 12, "bold"))
+        canvas.create_window(210, 380, window=post_label)
+        post_id = Entry(canvas, width=25, font=('', 14))
+        canvas.create_window(400, 380, window=post_id)
+        
+        create_button = Button(canvas, text="Create Video", bg="lime", font=("Adobe Garamond Pro", 12, "bold"), pady=8, command=lambda: fetch_post(subreddit_community.get(), post_id.get()))
+        canvas.create_window(350, 450, window=create_button)
+        
+        reddit_root.eval('tk::PlaceWindow . center')
+        reddit_root.mainloop()
+    else:
+        showerror("(!) Login Required (!)", "Please Login to use this feature!")
+    
 def save_reddit_info(client_id, client_secret):
     """ Store Reddit API credentials on the local device 
         for streamlined access and usage."""
@@ -105,12 +88,10 @@ def save_reddit_info(client_id, client_secret):
     if os.path.exists(db_name):
         conn = sqlite3.connect(db_name)
         c = conn.cursor()
-        c.execute(
-            "SELECT name FROM sqlite_master WHERE type='table' AND name=?", (table,))
+        c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name=?", (table,))
         table_exists = c.fetchone() is not None
         if table_exists:
-            c.execute("INSERT INTO users (client_id, client_secret) VALUES (?, ?)",
-                      (client_id, client_secret))
+            c.execute("INSERT INTO users (client_id, client_secret) VALUES (?, ?)", (client_id, client_secret))
             conn.commit()
         else:
             c.execute(f"CREATE TABLE {table} ({columns})")
@@ -118,104 +99,165 @@ def save_reddit_info(client_id, client_secret):
             conn.close()
             showinfo("(!) Login (!)", "Info Saved Successfully!")
             login_gui.destroy()
-            reddit_gui()
+            main_gui()
     else:
         showinfo("(!) Creating Database... (!)", "Database created!")
         conn = sqlite3.connect(db_name)
         save_reddit_info(client_id, client_secret)
-
-
+        
 def reddit_login_gui():
     """ GUI for storing Reddit API credentials """
     global login_gui
     login_gui = Tk()
     login_gui.title("Login")
-    login_gui.resizable(False, False)
-
+    login_gui.resizable(False,False)
+    
     canvas = Canvas(login_gui, bg=background, height=300, width=300)
     canvas.pack()
-
-    login_label = Label(canvas, text="Login", bg=background,
-                        font=('Adobe Garamond Pro', 20, "bold"))
+    
+    login_label = Label(canvas, text="Login", bg=background, font=('Adobe Garamond Pro', 20, "bold"))
     canvas.create_window(150, 30, window=login_label)
-
-    client_id_label = Label(canvas, text="Client ID:", bg=background, font=(
-        "Adobe Garamond Pro", 10, "bold"))
+    
+    client_id_label = Label(canvas, text="Client ID:", bg=background, font=("Adobe Garamond Pro", 10, "bold"))
     canvas.create_window(70, 100, window=client_id_label)
     client_id = Entry(canvas, width=25, font=('', 10))
     canvas.create_window(200, 100, window=client_id)
-
-    client_secret_label = Label(canvas, text="Client Secret:", bg=background, font=(
-        "Adobe Garamond Pro", 10, "bold"))
+    
+    client_secret_label = Label(canvas, text="Client Secret:", bg=background, font=("Adobe Garamond Pro", 10, "bold"))
     canvas.create_window(60, 150, window=client_secret_label)
     client_secret = Entry(canvas, width=25, font=('', 10))
     canvas.create_window(200, 150, window=client_secret)
 
-    save_button = Button(canvas, text="Save", bg="lime", font=('Adobe Garamond Pro', 10), pady=8,
-                         padx=10, command=lambda: (save_reddit_info(client_id.get().replace("\n", ""), client_secret.get().replace("\n", ""))))
+    save_button = Button(canvas, text="Save", bg="lime", font=('Adobe Garamond Pro', 10), pady=8, padx=10, command=lambda: (save_reddit_info(client_id.get(), client_secret.get())))
     canvas.create_window(150, 220, window=save_button)
-
+    
     login_gui.eval('tk::PlaceWindow . center')
     login_gui.mainloop()
-
-
-def reddit_login():
+    
+def reddit_login(gui):
     """ The system verifies the presence of Reddit API credentials for the user and, 
         if one is detected, proceeds with the next step. If a client ID is not present, 
         the user is prompted to provide one. """
     if os.path.exists(db_name):
-        conn = sqlite3.connect(db_name)
-        c = conn.cursor()
-        c.execute(f"SELECT * from {table}")
-        content = c.fetchone()
-        conn.close()
+        with sqlite3.connect(db_name) as conn:
+            c = conn.cursor()
+            c.execute(f"SELECT * from {table}")
+            content = c.fetchone()
         if content is not None:
             client_id = content[0]
-            opt = askyesno("Reddit API credentials",
-                           f"Currently Using:\nClient ID: {client_id}\nDo you want to continue?")
+            opt = askyesno("Reddit API credentials", f"Currently Using:\nClient ID: {client_id}\nDo you want to continue?")
             if opt:
                 root.destroy()
-                reddit_gui()
+                gui.destroy()
+                main_gui()
             else:
                 showwarning("Process Cancelled!", "Getting you Back....")
     else:
-        opt = askyesno("Reddit API credentials",
-                       "Currently We don't have your Reddit API credentials\nDo you wish to add API credentials")
+        opt = askyesno("Reddit API credentials", "Currently We don't have your Reddit API credentials\nDo you wish to add API credentials")
         if opt:
             root.destroy()
             reddit_login_gui()
-
+            
+def logout(gui):
+    os.remove("reddit_info.db")
+    gui.destroy()
+    main_gui()
 
 def main_gui():
     """ HOME Page """
     global root
     root = Tk()
-    root.title("Short Video Generator")
+    root.title("Text To Shorts")
     root.resizable(False, False)
-
-    canvas = Canvas(root, height=400, width=400, bg=background)
+    
+    canvas = Canvas(root, height=700, width=1000, bg=background)
     canvas.pack()
+    
+    header = Frame(canvas, bg="#159895", height=110, width=1010)
+    canvas.create_window(500, 50, window=header)
 
-    title = Label(canvas, text="Short Video\nGenerator",
-                  bg=background, font=('Terminal', 30, "bold"), fg="black")
-    canvas.create_window(200, 80, window=title)
-
-    opt = Label(canvas, text="Choose the platform:", bg=background, font=(
-        "Adobe Garamond Pro", 15, "bold underline"), fg="black")
-    canvas.create_window(200, 190, window=opt)
-
+    logo_img = Image.open('res/logo.png')
+    logo_img = logo_img.resize((80, 80))
+    logo = ImageTk.PhotoImage(logo_img)
+    logo_label = Label(canvas, image=logo, bg="#159895")
+    canvas.create_window(50, 50, window=logo_label)    
+    
+    title = Label(canvas, text="Text To Shorts\nEngine", bg=background, font=('Terminal', 32, "bold"), fg="black")
+    canvas.create_window(500, 200, window=title)
+    
+    opt = Label(canvas, text="Choose Your Platform", bg=background, font=("Adobe Garamond Pro", 15, "bold underline"), fg="black")
+    canvas.create_window(500, 280, window=opt)
+    
+    # open ai button
     openai = PhotoImage(file=os.path.join("res", "openai.png"))
     openai_button = Button(canvas, image=openai, height=80, width=80, border=0)
-    canvas.create_window(100, 260, window=openai_button)
-
+    canvas.create_window(360, 380, window=openai_button)
+    
+    # reddit button
     reddit = PhotoImage(file=os.path.join("res", "reddit.png"))
-    reddit_button = Button(canvas, image=reddit, height=80,
-                           width=80, border=0, bg=background, command=reddit_login)
-    canvas.create_window(300, 260, window=reddit_button)
+    reddit_button = Button(canvas, image=reddit, height=80, width=80, border=0, bg=background, command=reddit_gui)
+    canvas.create_window(640, 380, window=reddit_button)
 
+    # clip manager button
+    clip_manager_button = Button(canvas, text="Video Trimmer", fg="black", font=('', 15), height=2, width=20, bg="#10A37F", command=lambda: video.video_section(True))
+    canvas.create_window(700, 50, window=clip_manager_button)
+    
+    if os.path.isfile("reddit_info.db"):
+        logout_button = Button(canvas, text="Logout", fg="black", font=('', 15), height=2, width=10, bg="#10A37F", command=lambda: logout(root))
+        canvas.create_window(900, 50, window=logout_button)
+    else:
+        login_button = Button(canvas, text="Login", fg="black", font=('', 15), height=2, width=10, bg="#10A37F", command=lambda: reddit_login(root))
+        canvas.create_window(900, 50, window=login_button)
+    
     root.eval('tk::PlaceWindow . center')
     root.mainloop()
-
-
+    
 if __name__ == "__main__":
-    main_gui()
+    # make video directory if not present
+    if not os.path.exists("clips"):
+        os.mkdir("clips")
+    try:
+        import praw, prawcore
+        import video
+        from tkinter import *
+        from tkinter.messagebox import *
+        from PIL import ImageTk, Image
+        if os.name == "nt":
+            result = subprocess.run(["magick", "identify", "--version"], capture_output=True)
+        else:
+            result = subprocess.run(["convert", "--version"], capture_output=True)
+        main_gui()
+    except ModuleNotFoundError or FileNotFoundError:
+        try:
+            from tkinter import *
+            from tkinter.messagebox import *
+            from PIL import ImageTk, Image
+        except ModuleNotFoundError:
+            if os.name != "nt":
+                result = subprocess.run(["sudo", "apt-get", "install", "python-tk"])
+            result = subprocess.run(["pip", "install", "Pillow"])
+        opt = askokcancel("Install Library", "This Application requires python 'praw', 'OpenCV', 'gtts'  and 'MoviePY' libraries\nDo you wish to install it?")
+        if opt:
+            try:
+                if os.name == "nt":
+                    result = subprocess.run(["magick", "identify", "--version"], capture_output=True)
+                else:
+                    result = subprocess.run(["convert", "--version"], capture_output=True)
+            except FileNotFoundError:
+                # install ImageMagick since moviepy requires it
+                if os.name == "nt":
+                    result = subprocess.run(["winget", "install", "-e", "--id", "ImageMagick.ImageMagick"])
+                else:
+                    result = subprocess.run(["sudo", "apt-get", "install", "imagemagick"])
+            except:
+                showerror("Error!", "Error Occured!\nWhile installing 'ImageMagick' \nReport on issues section\nhttps://github.com/AroraKaran19/gpt-to-shorts/issues")
+            result = subprocess.run(["pip", "install", "-r", "requirements.txt"])
+            if result:
+                showinfo("Install Library", "(!) Successfully Installed (!)")
+                import praw, prawcore
+                import video
+                main_gui()
+            else:
+                showerror("Error!!", "Error Occured!\nWhile Installing from Requirements.txt\nReport on issues section\nhttps://github.com/AroraKaran19/gpt-to-shorts/issues")
+        else:
+            showinfo("(!) Exiting.. (!)", "Taking you back!")
